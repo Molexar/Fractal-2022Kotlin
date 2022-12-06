3f3e7b71aa4b114260f0f7c12790fef65f59c1bd
BadVibessz <danila.tsymbal@mail.ru>
2022-12-06 20:42:23 +0300

    added TODOs and some logic

---
+++ src/main/kotlin/ru/smak/video/VideoCreator.kt
@@ -0,0 +1,13 @@
+package ru.smak.video
+
+import jdk.jshell.spi.ExecutionControl.NotImplementedException
+import java.awt.image.BufferedImage
+
+object VideoCreator { // класс создания видео
+
+    fun CreateVideo(frames: List<BufferedImage>)
+    {
+        TODO("NOT IMPLEMENTED");
+    }
+
+}
--- src/main/kotlin/ru/smak/video/VideoSettings.kt
+++ src/main/kotlin/ru/smak/video/VideoSettings.kt
@@ -1,15 +1,29 @@
 package ru.smak.video
 
+import kotlinx.coroutines.DisposableHandle
+import ru.smak.graphics.Plane
 import java.awt.image.BufferedImage
 
-object VideoSettings {
+// класс параметров для создания видео
+object VideoSettings : DisposableHandle {
 
-    private val _keyShots = mutableListOf<BufferedImage>();
+    private val _statesList = mutableListOf<Plane>(); // список ключевых состояний (добавляются при нажатии на Add Shot кнопку)
 
-    fun GetKeyShotsCount() = _keyShots.size;
-    fun GetKeyShots() = _keyShots.toMutableList();
+    fun GetKeyShotsCount() = _statesList.size; // todo: maybe rename to GetKeyStatesCount?
+    fun GetKeyShots() = _statesList.toMutableList(); // same
 
-    fun AddKeyShot(shot: BufferedImage) = _keyShots.add(shot);
-    fun DeleteKeyShot(shot: BufferedImage) = _keyShots.remove(shot);
+    fun AddState(state: Plane) = _statesList.add(state);
+    fun DeleteState(state: Plane) = _statesList.remove(state);
+
+    override fun dispose() {
+        _statesList.clear();
+
+    }
+
+    // для последующего отображения на панельке списка ключевых кадров
+    // по нажатии на кнопку Show Shots
+    // todo: fun GetKeyFrames(_statesList) => list<BufferedImages>
+
+    // todo (для сани):  fun GetImagesByKeyStates(_statesList) => list<BufferedImages>
 
 }
