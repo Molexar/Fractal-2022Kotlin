525828524f6830afac8a61e7da7bc1b6e7d59ebd
BadVibessz <danila.tsymbal@mail.ru>
2022-12-07 21:59:37 +0300

    added Shot.kt and some modifies to VideoCreator.kt and VideoSettings.kt

--- src/main/kotlin/ru/smak/main.kt
+++ src/main/kotlin/ru/smak/main.kt
@@ -1,8 +1,47 @@
 package ru.smak
 
 import ru.smak.gui.MainWindow
+import ru.smak.video.VideoCreator
+import java.awt.image.BufferedImage
 
 fun main(args: Array<String>) {
-    MainWindow().isVisible = true
+    val w = MainWindow().apply { isVisible = true };
+    //MainWindow().isVisible = true
+
+
+    val screen = w.getScreenShot();
+
+    VideoCreator.createVideo(
+        "C:\\Users\\danil\\Documents\\video.mp4",
+        listOf(
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+            screen,
+        )
+    );
+
 }
 
