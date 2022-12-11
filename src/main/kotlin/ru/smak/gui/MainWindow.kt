7fd5141eae618e32bf7bef907ecef108974859b0
BadVibessz <danila.tsymbal@mail.ru>
2022-12-11 14:28:11 +0300

    synced fork

--- src/main/kotlin/ru/smak/gui/MainWindow.kt
+++ src/main/kotlin/ru/smak/gui/MainWindow.kt
@@ -21,8 +21,11 @@ import javax.swing.JFrame
 
 open class MainWindow : JFrame() {
     private var rect: Rectangle = Rectangle()
-    val minSz = Dimension(600, 450)
+    val minSz = Dimension(800, 600)
     val mainPanel: GraphicsPanel
+
+    private val _videoWindow = VideoWindow(this).apply { isVisible = false; };
+
     val trgsz = TargetSz()
 
     init {
@@ -46,241 +49,246 @@ open class MainWindow : JFrame() {
         mainPanel = GraphicsPanel().apply {
             background = Color.WHITE
             addPainter(fp)
-            //addPainter(fpj)
+            addComponentListener(object : ComponentAdapter() {
+                override fun componentResized(e: ComponentEvent?) {
+                    super.componentResized(e)
+                    plane.width = width
+                    plane.height = height
+                    makeOneToOne(plane, trgsz, mainPanel.size)//Делает панель мастштабом 1 к 1
+                }
+        })
+    }
 
+    menuBar.add(createRecordBtn(plane)); // создаем окошко для создания видео
+
+
+    mainPanel.addMouseListener(
+    object : MouseAdapter() {
+        override fun mouseClicked(e: MouseEvent?) {
+            super.mouseClicked(e)
+            SecondWindow(colorScheme).apply {
+                Julia.selectedPoint = Complex(Converter.xScrToCrt(e!!.x, plane), Converter.yScrToCrt(e.y, plane))
+                isVisible = true
+            }
+        }
+    })
+
+    mainPanel.addMouseListener(
+    object : MouseAdapter() {
+        override fun mousePressed(e: MouseEvent?) {
+            super.mousePressed(e)
+            e?.let {
+                rect.addPoint(it.point)
+            }
         }
 
-        mainPanel.addComponentListener(object : ComponentAdapter() {
-            override fun componentResized(e: ComponentEvent?) {
-                super.componentResized(e)
-                plane.width=width
-                plane.height=height
-                makeOneToOne(plane,trgsz, mainPanel.size)//Делает панель мастштабом 1 к 1
+        override fun mouseReleased(e: MouseEvent?) {
+            super.mouseReleased(e)
+            rect.leftTop?.let { first ->
+                val g = mainPanel.graphics
+                g.color = Color.BLACK
+                g.setXORMode(Color.WHITE)
+                g.drawRect(first.x, first.y, rect.width, rect.height)
+                g.setPaintMode()
+                if (rect.isExistst) {
+                    val x1 = rect.x1?.let { Converter.xScrToCrt(it, plane) } ?: return@let
+                    val x2 = rect.x2?.let { Converter.xScrToCrt(it, plane) } ?: return@let
+                    val y1 = rect.y1?.let { Converter.yScrToCrt(it, plane) } ?: return@let
+                    val y2 = rect.y2?.let { Converter.yScrToCrt(it, plane) } ?: return@let
+                    plane.xEdges = Pair(x1, x2)
+                    plane.yEdges = Pair(y1, y2)
+                    mainPanel.repaint()
+                }
             }
-        })
+            rect.destroy()
         }
+    })
 
-        mainPanel.addMouseListener(object: MouseAdapter(){
-            override fun mouseClicked(e: MouseEvent?) {
-                super.mouseClicked(e)
-                SecondWindow(colorScheme).apply {
-                    Julia.selectedPoint = Complex(Converter.xScrToCrt(e!!.x, plane), Converter.yScrToCrt(e.y, plane))
-                    isVisible = true
-                }
-            }
-        })
-
-        mainPanel.addMouseListener(object : MouseAdapter(){
-            override fun mousePressed(e: MouseEvent?) {
-                super.mousePressed(e)
-                e?.let {
-                    rect.addPoint(it.point)
-                }
-            }
-
-            override fun mouseReleased(e: MouseEvent?) {
-                super.mouseReleased(e)
-                rect.leftTop?.let {first->
+    mainPanel.addMouseMotionListener(
+    object : MouseAdapter() {
+        override fun mouseDragged(e: MouseEvent?) {
+            super.mouseDragged(e)
+            e?.let { curr ->
+                rect.leftTop?.let { first ->
                     val g = mainPanel.graphics
                     g.color = Color.BLACK
                     g.setXORMode(Color.WHITE)
-                    g.drawRect(first.x, first.y, rect.width, rect.height)
+                    if (rect.isExistst)
+                        g.drawRect(first.x, first.y, rect.width, rect.height)
+                    rect.addPoint(curr.point)
+                    rect.leftTop?.let { f -> g.drawRect(f.x, f.y, rect.width, rect.height) }
                     g.setPaintMode()
-                    if (rect.isExistst){
-                        val x1 = rect.x1?.let{Converter.xScrToCrt(it, plane)} ?: return@let
-                        val x2 = rect.x2?.let{Converter.xScrToCrt(it, plane)} ?: return@let
-                        val y1 = rect.y1?.let{Converter.yScrToCrt(it, plane)} ?: return@let
-                        val y2 = rect.y2?.let{Converter.yScrToCrt(it, plane)} ?: return@let
-                        makeOneToOne(plane,x1,x2,y1,y2,mainPanel.size,trgsz)//Делает панель мастштабом 1 к 1 и меняет trgsz
-                        mainPanel.repaint()
-                    }
-                }
-                rect.destroy()
-            }
-        })
-
-        mainPanel.addMouseMotionListener(object : MouseAdapter(){
-            override fun mouseDragged(e: MouseEvent?) {
-                super.mouseDragged(e)
-
-                e?.let{ curr->
-                    rect.leftTop?.let { first ->
-                        val g = mainPanel.graphics
-                        g.color = Color.BLACK
-                        g.setXORMode(Color.WHITE)
-                        if (rect.isExistst)
-                            g.drawRect(first.x, first.y, rect.width, rect.height)
-                        rect.addPoint(curr.point)
-                        rect.leftTop?.let { f -> g.drawRect(f.x, f.y, rect.width, rect.height) }
-                        g.setPaintMode()
-                    }
                 }
             }
-        })
+        }
+    })
 
+    layout = GroupLayout(contentPane).apply
+    {
+        setHorizontalGroup(
+            createSequentialGroup()
+                .addGap(8)
+                .addComponent(mainPanel, GROW, GROW, GROW)
+                .addGap(8)
+        )
+
+        setVerticalGroup(
+            createSequentialGroup()
+                .addGap(8)
+                .addComponent(mainPanel, GROW, GROW, GROW)
+                .addGap(8)
+        )
+    }
+}
+
+class AboutWindow : JFrame() {
+    val minSz = Dimension(400, 450)
+
+    val commonLabel: JLabel
+    val pplLabel1: JLabel
+    val pplLabel2: JLabel
+    val pplLabel3 = JLabel("Цымбал Данила");
+
+
+    init {
+        commonLabel = JLabel()
+        commonLabel.text = "Над проектом работали : "
+        pplLabel1 = JLabel()
+        pplLabel1.text = "Потасьев Никита"
+        pplLabel2 = JLabel()
+        pplLabel2.text = "Щербанев Дмитрий"
+
+        minimumSize = minSz
 
         layout = GroupLayout(contentPane).apply {
             setHorizontalGroup(
                 createSequentialGroup()
                     .addGap(8)
-                    .addComponent(mainPanel, GROW, GROW, GROW)
+                    .addGroup(
+                        createParallelGroup()
+                            .addComponent(commonLabel, SHRINK, SHRINK, SHRINK)
+                    )
+                    .addGap(16)
+                    .addGroup(
+                        createParallelGroup()
+                            .addComponent(pplLabel1, SHRINK, SHRINK, SHRINK)
+                            .addComponent(pplLabel2, SHRINK, SHRINK, SHRINK)
+                            .addComponent(pplLabel3, SHRINK, SHRINK, SHRINK)
+                    )
                     .addGap(8)
             )
 
             setVerticalGroup(
                 createSequentialGroup()
                     .addGap(8)
-                    .addComponent(mainPanel, GROW, GROW, GROW)
+                    .addGroup(
+                        createParallelGroup()
+                            .addComponent(commonLabel, SHRINK, SHRINK, SHRINK)
+                            .addComponent(pplLabel1, SHRINK, SHRINK, SHRINK)
+                    )
+                    .addGroup(
+                        createParallelGroup()
+                            .addComponent(pplLabel2, SHRINK, SHRINK, SHRINK)
+                    )
+                    .addGroup(
+                        createParallelGroup()
+                            .addComponent(pplLabel3, SHRINK, SHRINK, SHRINK)
+                    )
                     .addGap(8)
             )
         }
-
-        this.addKeyListener(object : KeyAdapter(){
-            override fun keyPressed(e: KeyEvent?) {
-                super.keyPressed(e)
-                if (e != null)
-                    if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_O)
-                    {
-                        VideoWindow();
-                    }
-            }
-        })
-
     }
 
-    class AboutWindow : JFrame() {
-        val minSz = Dimension(400, 450)
 
-        val commonLabel: JLabel
-        val pplLabel1: JLabel
-        val pplLabel2: JLabel
-        val pplLabel3 = JLabel("Цымбал Данила");
+}
 
+private fun createAboutButton(): JButton {
+    val aboutButton = JButton("О программе")
+    aboutButton.addMouseListener(object : MouseAdapter() {
+        override fun mousePressed(e: MouseEvent?) {
+            super.mousePressed(e)
+            e?.let {
+                val frame = AboutWindow()
+                frame.isVisible = true
+                frame.defaultCloseOperation = DISPOSE_ON_CLOSE
 
-        init {
-            commonLabel = JLabel()
-            commonLabel.text = "Над проектом работали : "
-            pplLabel1 = JLabel()
-            pplLabel1.text = "Потасьев Никита"
-            pplLabel2 = JLabel()
-            pplLabel2.text = "Щербанев Дмитрий"
-
-            minimumSize = minSz
-
-            layout = GroupLayout(contentPane).apply {
-                setHorizontalGroup(
-                    createSequentialGroup()
-                        .addGap(8)
-                        .addGroup(
-                            createParallelGroup()
-                                .addComponent(commonLabel, SHRINK, SHRINK, SHRINK)
-                        )
-                        .addGap(16)
-                        .addGroup(
-                            createParallelGroup()
-                                .addComponent(pplLabel1, SHRINK, SHRINK, SHRINK)
-                                .addComponent(pplLabel2, SHRINK, SHRINK, SHRINK)
-                                .addComponent(pplLabel3, SHRINK, SHRINK, SHRINK)
-                        )
-                        .addGap(8)
-                )
-
-                setVerticalGroup(
-                    createSequentialGroup()
-                        .addGap(8)
-                        .addGroup(
-                            createParallelGroup()
-                                .addComponent(commonLabel, SHRINK, SHRINK, SHRINK)
-                                .addComponent(pplLabel1, SHRINK, SHRINK, SHRINK)
-                        )
-                        .addGroup(
-                            createParallelGroup()
-                                .addComponent(pplLabel2, SHRINK, SHRINK, SHRINK)
-                        )
-                        .addGroup(
-                            createParallelGroup()
-                                .addComponent(pplLabel3, SHRINK, SHRINK, SHRINK)
-                        )
-                        .addGap(8)
-                )
             }
         }
-    }
+    })
+    return aboutButton
 
-    private fun createAboutButton(): JButton {
-        val aboutButton = JButton("О программе")
-        aboutButton.addMouseListener(object : MouseAdapter() {
-            override fun mousePressed(e: MouseEvent?) {
-                super.mousePressed(e)
-                e?.let {
-                    val frame = AboutWindow()
-                    frame.isVisible = true
-                    frame.defaultCloseOperation = DISPOSE_ON_CLOSE
+}
 
+
+private fun createColorMenu(): JMenu {
+    val colorMenu = JMenu("Выбор цветовой гаммы")
+
+    val mClr = JColorChooser()
+    val sClr = JColorChooser()
+
+    var firstColor: Color
+    var secondColor: Color
+
+    firstColor = mClr.selectionModel.selectedColor
+    secondColor = sClr.selectionModel.selectedColor
+
+    colorMenu.add(mClr)
+    colorMenu.add(sClr)
+
+
+    return colorMenu
+
+}
+
+private fun createCtrlZButton(): JButton {
+    val ctrlzButton = JButton("Отменить предыдущее действие")
+
+    return ctrlzButton
+
+}
+
+private fun createRecordBtn(plane: Plane): JButton {
+    val btn = JButton("Record");
+
+    btn.addMouseListener(object : MouseAdapter() {
+        override fun mousePressed(e: MouseEvent?) {
+            super.mousePressed(e)
+            e?.let {
+                _videoWindow.apply {
+                    this.plane = plane;
+                    isVisible = true
                 }
             }
-        })
-        return aboutButton
-
-    }
-
-
-    private fun createColorMenu(): JMenu {
-        val colorMenu = JMenu("Выбор цветовой гаммы")
-
-        val mClr = JColorChooser()
-        val sClr = JColorChooser()
-
-        var firstColor: Color
-        var secondColor: Color
-
-        firstColor = mClr.selectionModel.selectedColor
-        secondColor = sClr.selectionModel.selectedColor
-
-        colorMenu.add(mClr)
-        colorMenu.add(sClr)
-
-
-        return colorMenu
-
-    }
-
-    private fun createCtrlZButton(): JButton {
-        val ctrlzButton = JButton("Отменить предыдущее действие")
-
-        return ctrlzButton
-
-    }
-
-    private fun createRecordBtn(): JButton
-    {
-        val btn = JButton("Record");
-
-        btn.addMouseListener(object : MouseAdapter() {
-            override fun mousePressed(e: MouseEvent?) {
-                super.mousePressed(e)
-                e?.let {
-                    val frame = VideoWindow()
-                    frame.isVisible = true
-                    frame.defaultCloseOperation = DISPOSE_ON_CLOSE
-
-                }
-            }
-        })
-        return btn;
-    }
-
-    override fun setVisible(b: Boolean) {
-        super.setVisible(b)
-        mainPanel.graphics.run {
-            setXORMode(Color.WHITE)
-            drawLine(-100, -100, -101, -100)
-            setPaintMode()
         }
-    }
+    })
+    return btn;
+}
 
-    companion object {
-        const val GROW = GroupLayout.DEFAULT_SIZE
-        const val SHRINK = GroupLayout.PREFERRED_SIZE
+override fun setVisible(b: Boolean) {
+    super.setVisible(b)
+    mainPanel.graphics.run {
+        setXORMode(Color.WHITE)
+        drawLine(-100, -100, -101, -100)
+        setPaintMode()
     }
+}
+
+companion object {
+    const val GROW = GroupLayout.DEFAULT_SIZE
+    const val SHRINK = GroupLayout.PREFERRED_SIZE
+}
+
+// TODO: for testing video creation
+fun getScreenShot(width: Int, height: Int): BufferedImage {
+
+    val image = BufferedImage(
+        width,
+        height,
+        BufferedImage.TYPE_INT_RGB
+    )
+    mainPanel.paint(image.graphics)
+    return image
+}
+
+
 }
